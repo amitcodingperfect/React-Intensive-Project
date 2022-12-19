d18d42b563ccbec4e99ee41dabd320ecee5a279e
amit <amitjha6358@gmail.com>
2022-12-20 01:37:32 +0530

    twitter-revised

--- twitter/src/components/ProfileMain/ProfileMain.js
+++ twitter/src/components/ProfileMain/ProfileMain.js
@@ -1,22 +1,57 @@
 
-import "./ProfileMain.css";
+
+import './ProfileMain.css'
+import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import Trending from '../Feed/FeedHome/Trending';
+import Tweet from '../Tweet/Tweet';
 
 function ProfileMain(){
-
-    return(
-        <div className="profile-main">
-            <div className="cover">
-                <a href="#">
-                <img className="profile-img" src="https://pbs.twimg.com/profile_images/1590968738358079488/IY9Gx6Ok_400x400.jpg"/>    
-                </a>
+    const [trendings, setTrendings]=useState([]);
+    const [tweets, setTweets]=useState([]);
+    useEffect(()=>{
+        fetch("trendingData.json" ).then(res=> res.json()).then(res=>{
+                   
+                    setTrendings(res);
+                    console.log(res);
         
-            </div>
-         <div className="follow-btn-container fw-bold float-end">
-            <button className="follow-btn">Follow</button>
-            </div>   
+                });
+        },[]);
+        
+        useEffect(()=>{
+            fetch("tweet.json" ).then(res=> res.json()).then(res=>{
+                       
+                        setTweets(res);
+                        console.log(res);
             
-        </div>
-    )
+                    });
+            },[]);
+    return (
+       <div className="profile-main">
+       <div className="cover">
+       <img className="profile-img" src="https://pbs.twimg.com/profile_images/1590968738358079488/IY9Gx6Ok_400x400.jpg" alt="profile"></img>
+       </div>
+       <div className="follow-btn-container fw-bold float-end">
+            <button className="follow-btn">Follow</button>
+            </div> 
+            <div className='trend-class'>
+            {
+            trendings.map(trending=>
+                <Trending data={trending} />
+            )}
 
-} 
+            <hr />
+            {
+            tweets.map(tweet=>
+                <div>
+                <Tweet data={tweet} />
+                </ div>
+                  
+            )}
+          </div>
+       </div>
+
+    )
+}
 export default ProfileMain;
